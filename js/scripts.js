2efb4ede3d59305c79f4f0aa62e7ccadc98158ea
Noah Lundquist <nalundquist@gmail.com>
2022-08-26 11:29:44 -0700

    add js for form collection/display

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,51 @@ Pizza.prototype.priceTab = function() {
 		}
 	}
 }
+
+// User Interface Logic
+
+function formSubmitHandler(event){
+	event.preventDefault();
+
+	const sizeSelect = document.getElementById("pizza-size");
+	const topSubmit = document.getElementById("top-submit-1");
+
+	sizeSelect.addEventListener("select", function(){
+		topSubmit.removeAttribute("class", "hidden");
+	})
+
+	const size = document.getElementById("pizza-size").value;
+	const toppings = document.querySelectorAll("input[name=toppings]:checked");
+	
+	let pizza = new Pizza(size, toppings);
+	pizza.priceTab();
+	
+	const h1 = document.createElement("h1");
+	const sizeh3 = document.createElement("h3");
+	const toph3 = document.createElement("h3");
+	const priceh3 = document.createElement("h3");
+	const sizeh4 = document.createElement("h4");
+	const toph4 = document.createElement("h4");
+	const priceh4 = document.createElement("h4");
+	const orderBox = document.getElementById("order-summary");
+
+	orderBox.append(h1);
+	h1.append("Pizza Ordered!");
+	sizeh3.after(h1);
+	sizeh3.append("Size: ");
+	sizeh4.after(sizeh3);
+	sizeh4.append(pizza.size);
+	toph3.after(sizeh4);
+	toph3.append("Toppings: ");
+	toph4.after(toph3);
+	toph4.append(pizza.toppings);
+	priceh3.after(toph4);
+	priceh3.append("Total: $");
+	priceh4.after(priceh3);
+	priceh4.append(pizza.price);
+}
+
+window.addEventListener("load", function(){
+	let form = document.getElementById("order-form");
+	form.addEventListener("submit", formSubmitHandler);
+})
