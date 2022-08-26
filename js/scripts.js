9a66cd06c488495ccad5aac097911f78813e9019
Noah Lundquist <nalundquist@gmail.com>
2022-08-26 10:00:25 -0700

    fulfill second priceTab test, create third for bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -9,40 +9,15 @@ function Pizza(toppings, size){
 Pizza.prototype.priceTab = function() {
 	if (this.size === "small"){
 		this.price += 8.99;
-		this.toppings.map(function(i){
-			this.price += 1.99;
-			if (i === "sirloin" || i === "escargot" || i === "chevre" || i === "black truffle"){
-				this.price += 1.99;
-			}
-			return this.price;
-		})
+		this.price = this.price + (2.49 * this.toppings.length)
 	} else if (this.size === "medium"){
 		this.price += 10.99;
-		this.toppings.map(function(i){
-			this.price += 2.99;
-			if (i === "sirloin" || i === "escargot" || i === "chevre" || i === "black truffle"){
-				this.price += 2.99;
-			}
-			return this.price;
-		})
-	} else if (this.size === "large"){
+		this.price = this.price + (2.99 * this.toppings.length)
+		} else if (this.size === "large"){
 		this.price += 13.99;
-		this.toppings.map(function(i){
-			console.log("inside toppings map loop");
-			this.price += 3.99;
-			if (i === "sirloin" || i === "escargot" || i === "chevre" || i === "black truffle"){
-				this.price += 3.99;
-			}
-			return this.price;
-		})
+		this.price = this.price + (3.99 * this.toppings.length)
 	} else {
 		this.price += 16.99;
-		this.toppings.map(function(i){
-			this.price += 5.49;
-			if (i === "sirloin" || i === "escargot" || i === "chevre" || i === "black truffle"){
-			this.price += 5.49;
-			}
-			return this.price;
-		})
+		this.price = this.price + (5.49 * this.toppings.length)
 	}
 }
